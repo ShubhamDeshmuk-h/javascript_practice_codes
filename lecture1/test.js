6628cc8f0f2694a08ee5be05db55b9f234e20852
ShubhamDeshmuk-h <shubhamdeshmukh3251@gmail.com>
2025-04-14 01:34:42 +0530

    variables and cont

--- lecture1/test.js
+++ lecture1/test.js
@@ -1,1 +1,14 @@
 console.log("Shubham")
+
+//for lecture one we are going to learn about variables and data types
+// variables are used to store data in memory
+// data types are used to define the type of data that can be stored in a variable
+// there are 7 data types in javascript
+// 1. string - used to store text data  
+// 2. number - used to store numeric data
+// 3. boolean - used to store true or false data
+// 4. object - used to store complex data
+// 5. array - used to store a collection of data
+// 6. null - used to store no value
+// 7. undefined - used to store a variable that has no value
+
