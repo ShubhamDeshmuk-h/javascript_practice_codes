0ec659a3599ee4a9f23aa5d22c569acf9d8e2448
ShubhamDeshmuk-h <shubhamdeshmukh3251@gmail.com>
2025-04-14 20:16:49 +0530

    1:22:16

--- lecture2/dataconversion.js
+++ lecture2/dataconversion.js
@@ -17,7 +17,31 @@ console.log(booleanIsloggedin);
 
 
 //number to string and bool to string
+
+
 let somenumber = 33
 let convertedString = String(somenumber)
-console.log(typeof convertedString);
-console.log(typeof somenumber);
+// console.log(typeof convertedString);
+// console.log(typeof somenumber);
+
+
+// operations
+
+
+let str = "hello"
+let str2 = "shubham"
+
+let str3 = str + str2
+console.log(str3);
+
+// for console.log("1"+2);
+//for console.log(1+"2");
+//for the above operation output is 12 how is this occure now
+
+
+//for console.log(1+2+"2");
+// output is 32
+
+//for console.log("1"+2+2);
+// output is 122
+
