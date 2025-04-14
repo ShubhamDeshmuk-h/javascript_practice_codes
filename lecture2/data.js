6b858991c3e505ececa4767ec2bd64fae4c4357e
ShubhamDeshmuk-h <shubhamdeshmukh3251@gmail.com>
2025-04-14 19:57:50 +0530

    1:01:46

---
+++ lecture2/data.js
@@ -0,0 +1,33 @@
+"use strict";
+//code is treated with newer version of the code 
+//use strict is used to avoid errors in the code
+
+//alert("Hello world");
+
+//alert("1+2");
+
+console.log("Hello world");
+
+//always make the code readable 
+
+let name = "Ashish"
+let age = 18
+let isloggedin = false
+/* premetrive datatype
+
+//number => 2 to power 53
+//bigin
+//string =>
+//boolean => true or false
+//null => standalone value
+//undefined =>
+//symbol => to find uniqueness we use this to make it unique
+
+
+what is null it is object 
+
+
+*/
+
+console.log(typeof null);
+
